aafacdb65a349bbb10719b6408f60633587916d5
sarahsululu <sarahsululu47@gmail.com>
2025-08-27 15:12:55 +0300

    Add settings module with configuration constants

---
+++ charity_donation/settings.py
@@ -0,0 +1,13 @@
+
+
+CLI_BANNER = """
+===============================
+   CHARITY DONATION PLATFORM
+===============================
+"""
+
+MIN_DONATION_AMOUNT = 1
+MAX_DONATION_AMOUNT = 10000
+
+MIN_CAMPAIGN_GOAL = 50
+MAX_CAMPAIGN_GOAL = 50000
